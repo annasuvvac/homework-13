f397e3b2c11b89c46528d3e08dfabc2149997cec
Ana Suvac <anasuvac@yahoo.com>
2020-01-25 19:37:50 -0500

    fix errors

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+insert into burgers (burger_name)
+values ("SOCAL Burger");
+insert into burgers (burger_name)
+values ("Cowboy Burger");
+insert into burgers (burger_name)
+values ("Elvis Burger");
+insert into burgers (burger_name)
+values ("Turkey Burger");
+insert into burgers (burger_name)
+values ("Aussie Burger");
