7c7b3d7282c177ba341571a2a740ca4045caea3e
Ana Suvac <anasuvac@yahoo.com>
2020-01-25 20:05:58 -0500

    fix errors

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,6 @@
 var mysql = require("mysql");
 
 var connection;
-// = mysql.createConnection({
-//   host: "localhost",
-//   port: 3306,
-//   user: "root",
-//   password: "Monday15!",
-//   database: "burgers_db"
-// });
 
 if (process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
@@ -23,13 +16,4 @@ if (process.env.JAWSDB_URL){
 
 // Make connection.
 connection.connect();
-  // function(err) {
-  //   if (err) {
-  //     console.error("error connecting: " + err.stack);
-  //     return;
-  //   }
-  //   console.log("connected as id " + connection.threadId);
-  // });
-  
-  // Export to ORM to use.
   module.exports = connection;
