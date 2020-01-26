6ffe762c2acb11b5c9fdf501aed3b739624e53e5
Ana Suvac <anasuvac@yahoo.com>
2020-01-25 20:03:05 -0500

    fix error

--- public/js/burgerdisplay.js
+++ public/js/burgerdisplay.js
@@ -1,32 +1,29 @@
-// Make sure we wait to attach our handlers until the DOM is fully loaded.
+
 $(function () {
     $(".devoured").on("click", function (event) {
         var id = $(this).data("id");
         if ($(this).data("devoured") === 0){
            var devoured = true
         }else devoured = false
-        // var devoured = $(this).data("devoured");
+     
         console.log(devoured);
         console.log(id);
         var newDevoured = {
             devoured: devoured
         };
 
-        // Send the PUT request.
         $.ajax("/api/burgers/" + id, {
             type: "PUT",
             data: newDevoured
         }).then(
             function () {
                 console.log("changed devoured status to ", devoured);
-                // Reload the page to get the updated list
                 location.reload();
             }
         );
     });
 
     $(".create-form").on("submit", function (event) {
-        // Make sure to preventDefault on a submit event.
         event.preventDefault();
 
         var newBurger = {
